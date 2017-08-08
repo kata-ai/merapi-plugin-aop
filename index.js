b9870aa8a63289dc4e819748344e9e0dac34a8ff
Ricky Anders <ricky@yesbossnow.com>
2017-08-08 21:54:08 +0700

    Add aop handler on component instantiate

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 "use strict";
 
-module.exports = function () {
+const { utils } = require("merapi");
+
+module.exports = function (container) {
     return {
-        dependencies: []
+        dependencies: [],
+
+        *onBeforeComponentsRegister() {
+            container.register("aop", require("./lib/aop"));
+        },
+
+        *onComponentInstantiate(name, component) {
+            let aop = yield container.resolve("aop");
+            let matches = aop.matchComponent(name);
+            if (matches) {
+                for (let i = 0; i < matches; i++) {
+                    utils.getAllPropertyNames(component).forEach(prop => {
+                        if ((matches[i].methodName === "*" || prop === matches[i].methodName)
+                            && typeof component[prop] === "function") {
+                                let handler = yield container.injector.resolveMethod(matches[i].handler);
+                                component[prop] = aop.wrapMethod(handler, component, prop, matches[i].options);
+                            }
+                    });
+                }
+            }
+        }
     };
 }
