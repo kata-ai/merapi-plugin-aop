2132ad3d39fc81e79aca8aea45aeae3f07f23fac
Ricky Anders <rickyanders93@gmail.com>
2017-08-09 12:54:50 +0700

    Avoid process aop component and minor fix in wrap method

--- index.js
+++ index.js
@@ -12,16 +12,19 @@ module.exports = function (container) {
 
         *onComponentInstantiate(name, component) {
             let aop = yield container.resolve("aop");
-            let matches = aop.matchComponent(name);
-            if (matches) {
-                for (let i = 0; i < matches; i++) {
-                    utils.getAllPropertyNames(component).forEach(function* (prop) {
-                        if ((matches[i].methodName === "*" || prop === matches[i].methodName)
-                            && typeof component[prop] === "function") {
-                                let handler = yield container.injector.resolveMethod(matches[i].handler);
-                                component[prop] = aop.wrapMethod(handler, component, prop, matches[i].options);
-                            }
-                    });
+            if(name !== "aop") {
+                let matches = aop.matchComponent(name);
+                if (matches) {
+                    for (let i = 0; i < matches.length; i++) {
+                        let props = utils.getAllPropertyNames(component);
+                        for (let j = 0; j < properties.length; j++) {
+                            if ((matches[i].methodName === "*" || props[j] === matches[i].methodName)
+                                && typeof component[props[j]] === "function") {
+                                    let handler = yield container.injector.resolveMethod(matches[i].handler);
+                                    component[props[j]] = yield aop.wrapMethod(handler, component, props[j], matches[i].options);
+                                }
+                        }
+                    }
                 }
             }
         }
